6d8ac6efcf5ecbfaa82634afbc4ce35a33a3eec3
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2016-02-18 20:41:35 +0900

    integrated on Angular.

--- index.js
+++ index.js
@@ -1,40 +1,73 @@
+'use strict';
+
 var remote = require('remote');
 var converter = remote.require('./build/Release/converter');
-var app = remote.require('app');
-var BrowserWindow = remote.require('browser-window');
-var dialog = remote.require('dialog');
 
-var src_file = "";
-var dst_file = "";
+var SP360Converter = angular.module('SP360Converter', ['ngElectron']);
 
-function openFile() {
-    var options = {};
-    dialog.showOpenDialog(BrowserWindow.getFocusedWindow(), options, function(filenames){
-        src_file = filenames[0];
-    });
-}
+SP360Converter.controller('MainController', ['$scope', '$q', 'electron',
+function($scope, $q, electron) {
+    var BrowserWindow = electron.browserWindow;
+    var Dialog = electron.dialog;
 
-function convert() {
-    if (!src_file) {
-        return
-    }
-    var options = {};
-    dialog.showSaveDialog(BrowserWindow.getFocusedWindow(), options, function(filename){
-        dst_file = filename;
-        converter.convert({
-            src_file: src_file,
-            dst_file: dst_file,
-            start_time:  0,
-            end_time:    1000,
-            dst_width:   1280,
-            dst_height:  720,
-            n_split:     2,
-            start_theta: 0.0 * Math.PI,
-            end_theta:   2.0 * Math.PI
-        }, function(err, status, progress){
-            console.log(err);
-            console.log(status);
-            console.log(progress);
+    $scope.src_file = "";
+    $scope.dst_file = "";
+    $scope.convert_progress = 0.0;
+
+    $scope.openFile = function() {
+        var deferred = $q.defer()
+        var options = {};
+        Dialog.showOpenDialog(BrowserWindow.getFocusedWindow(), options, function(filenames){
+            if (filenames) {
+                deferred.resolve(filenames);
+            }
         });
-    });
-}
+        deferred.promise.then(function(filenames){
+            $scope.src_file = filenames[0];
+        });
+    };
+
+    $scope.convert = function() {
+        if (!$scope.src_file) {
+            return
+        }
+        var deferred = $q.defer()
+        var options = {};
+        Dialog.showSaveDialog(BrowserWindow.getFocusedWindow(), options, function(filename){
+            $scope.dst_file = filename;
+            if (!$scope.src_file || !$scope.dst_file) {
+                return;
+            }
+            converter.convert({
+                src_file:    $scope.src_file,
+                dst_file:    $scope.dst_file,
+                start_time:  0,
+                end_time:    1000,
+                dst_width:   1280,
+                dst_height:  720,
+                n_split:     2,
+                start_theta: 0.0 * Math.PI,
+                end_theta:   2.0 * Math.PI
+            }, function(err, status, progress){
+                console.log(err, status, progress);
+                if (err) {
+                    deferred.reject(err);
+                } else if (status == "progress") {
+                    deferred.notify(progress);
+                } else if (status == "successed") {
+                    deferred.resolve();
+                } else {
+                    deferred.reject("invalid response.");
+                }
+            });
+        });
+        deferred.promise.then(function(){
+            $scope.converter_progress = 0.0;
+        }, function(error){
+            console.error(error);
+        }, function(progress){
+            $scope.convert_progress = progress;
+        });
+
+    }
+}]);
