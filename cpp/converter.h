174df7a7f59b5cd3ad689e329d9ee6c1ea77d016
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2016-02-28 19:24:42 +0900

    use class.

--- cpp/converter.h
+++ cpp/converter.h
@@ -7,13 +7,19 @@
 
 namespace SP360
 {
-    int open(std::string src_file);
+    class Converter {
+    private:
+        cv::VideoCapture videoCapture;
+        cv::Mat previewImage;
 
-    int makeImage(std::string src_file, unsigned char* dst_array,
+    public:
+        int open(std::string src_file);
+
+        int makeImage(std::string src_file, unsigned char* dst_array,
                   unsigned int dst_width, unsigned int dst_height,
                   unsigned int time);
 
-    int makeConvertBorderImage(std::string src_file, unsigned char* dst_array,
+        int makeConvertBorderImage(std::string src_file, unsigned char* dst_array,
                                unsigned int dst_width, unsigned int dst_height,
                                unsigned int time,
                                double angle_start, double angle_end,
@@ -22,7 +28,7 @@ namespace SP360
                                int n_points_w, int n_points_h);
 
 
-    int makeConvertedImage(std::string src_file, unsigned char* dst_array,
+        int makeConvertedImage(std::string src_file, unsigned char* dst_array,
                            unsigned int dst_width, unsigned int dst_height,
                            unsigned int time,
                            double angle_start, double angle_end,
@@ -30,12 +36,13 @@ namespace SP360
                            int n_split);
 
 
-    int convertMovie(std::string src_file, std::string dst_file,
+        int convertMovie(std::string src_file, std::string dst_file,
                      unsigned int dst_width, unsigned int dst_height,
                      unsigned int start_time, unsigned int end_time,
                      double angle_start, double angle_end,
                      double radius_in, double radius_out,
                      int n_split,
                      std::function<void(float)> callback);
+    };
 }
 #endif // CONVERTER_H
